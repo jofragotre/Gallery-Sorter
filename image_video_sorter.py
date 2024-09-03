138550e1ea03f1b745b54d442c2174e2ee8a55a8
JoaoTremoco <joao.tremoco@yoonik.me>
2024-09-03 17:24:54 +0100

    preparation for v2

--- image_video_sorter.py
+++ image_video_sorter.py
@@ -5,15 +5,15 @@ Gallery date sorter
 import argparse
 from utils.utils import iterate_root_folder, process_file
 
-# TODO: Add support to filter just a single folder for speed-up
-# TODO: Add support for image/video separate folders
+# TODO: Add support for diferent input and output folders
 # TODO: Add support for multiprocessing
 # TODO: Add duplicator remover
 # TODO: Make executable file
+# TODO: Make GUI
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--folder", help="Folder to process", required=True)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    iterate_root_folder(args.root_path, process_file)
+    iterate_root_folder(args.folder, process_file)
